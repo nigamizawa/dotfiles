0fff3d144000bae7a95fadf151f0750e666f573d
苦味沢 <nigamizawa@gmail.com>
2021-08-10 22:51:00 +0900

    [fix]

--- init.sh
+++ init.sh
@@ -2,14 +2,8 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-add submodule
+# add submodule
 git submodule update --init --recursive
-
-# prezto
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
 
 # add config folders
 folders=(
